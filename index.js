5eee01d230e4750a03b6aade7e5953c404d9a45e
Barry Fisher <barry.fisher@reallifedigital.com>
2017-11-14 00:08:32 +0000

    Greater simplification of options + inherited defaults

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import panacea from '@panaceajs/core'
 
 // See documentation for full list of options.
-const options = {
-  main: { deferListen: true }
-}
-
-panacea(options).then(app => {
-
-  // Add other express middleware here if required.
-
-  // Start the express app listening on port 3000.
-  app.listen(3000) && console.log(`Server listening on port 3000`)
-
-}).catch(error => console.log(`An error occurred: ${error}`))
+panacea()
