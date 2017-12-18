227c9850f8c38f8f1ee5aa795c58a479922ae6d6
Barry Fisher <barry.fisher@reallifedigital.com>
2017-12-18 14:56:48 +0000

    Updates npm start scripts to build cms on npm start. Fixes publicPath with trailing slash.

--- panacea.js
+++ panacea.js
@@ -7,7 +7,7 @@ export default function () {
       },
       build: {
         // The path where the CMS should load.
-        publicPath: '/cms'
+        publicPath: '/cms/'
       }
     },
     plugins: [
