882fbe22d4865693ed4692f7cadb027229099dd9
Barry Fisher <barry.fisher@reallifedigital.com>
2018-01-20 01:44:49 +0000

    Updates custom-path example to work with translations

---
+++ cms/pages/_lang/custom-path/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="title">{{ $t('starter-app.h1') }}</h1>
+    <p>{{ $t('starter-app.description') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.$t("starter-app.title"),
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$t("starter-app.description")
+        }
+      ]
+    };
+  }
+};
+</script>
