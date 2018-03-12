195899dba430fc958e00f1bf591e35903cba2f9a
Barry Fisher <barry.fisher@reallifedigital.com>
2018-03-12 16:14:45 +0000

    Updates index.js as panacea() no longer returns app, but instead makes it available on the container.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import panacea from '@panaceajs/core'
 import cmsMiddleware from '@panaceajs/cms/expressMiddleware'
 
-panacea().then(config => {
-  const { app, options } = config
+panacea().then(() => {
+  const { app, options } = DI.container
   cmsMiddleware(app, options)
   app.listen(`${options.main.port}`)
   console.log(`Server started. Listening on port ${options.main.port}`)
