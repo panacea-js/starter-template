8cb48f7a4ed7697c6892a5357feb39afe891ae4a
Barry Fisher <barry.fisher@reallifedigital.com>
2018-05-07 22:50:07 +0100

    Adds CORS hook to whitelist origins matching the graphql server (core) and port 3001 for dev:cms script.

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,7 +1,14 @@
 export default {
   register: function (hooks) {
-    hooks.on('core.graphql.definitions.types', data => {
-      // console.log(data)
+    hooks.on('core.cors.whitelist', ({ whitelist, options }) => {
+      // Allow access on to origin as set in .env file.
+      // This allows production access to the graphql server by graphiql and voyager.
+      whitelist.push(`${options.main.protocol}://${options.main.host}:${options.main.port}`)
+      // Allow access on to origin as set in .env file, but with port 3001 for cms development.
+      whitelist.push(`${options.main.protocol}://${options.main.host}:3001`)
+
+      // Optional - allow access from all hosts.
+      //whitelist.push('*')
     })
   }
 }
