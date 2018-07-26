81cf1c3a69dfb80b66b1df168677f87fdc5ab7c5
Barry Fisher <barry.fisher@reallifedigital.com>
2018-07-26 17:38:52 +0100

    Add firstName and lastName fields in hooks file to demonstrate extending existing entity fields.

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -8,7 +8,30 @@ export default {
       whitelist.push(`${options.main.protocol}://${options.main.host}:3001`)
 
       // Optional - allow access from all hosts.
-      //whitelist.push('*')
+      // whitelist.push('*')
+    })
+
+    hooks.on('core.entities.definitions', entityTypes => {
+      // Reference back to this file to improve debugging experience.
+      const hookFile = __filename
+
+      // Extend user entity with custom fields.
+      // Ensure that the field key (e.g. firstName) is camelCase.
+      entityTypes.User.fields.firstName = {
+        label: 'First Name',
+        type: 'string',
+        description: `The user's last name`,
+        required: true,
+        _meta: { hookFile }
+      }
+
+      entityTypes.User.fields.lastName = {
+        label: 'Last Name',
+        type: 'string',
+        description: `The user's first name`,
+        required: true,
+        _meta: { hookFile }
+      }
     })
   }
 }
