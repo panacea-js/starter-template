92d2b22f11892f778f87929af0996c85ebefc84a
Barry Fisher <barry.fisher@reallifedigital.com>
2018-09-16 01:05:00 +0100

    Add check in index.js for availability of the express app object- in that case that Panacea core didn't load correctly.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import cmsMiddleware from '@panaceajs/cms/expressMiddleware'
 
 panacea().then(() => {
   const { app, options } = Panacea.container
+  if (!app) {
+    console.error('There was an error loading Panacea core')
+    return
+  }
   cmsMiddleware(app, options)
   app.listen(`${options.main.port}`)
   console.log(`Server started. Listening on port ${options.main.port}`)
